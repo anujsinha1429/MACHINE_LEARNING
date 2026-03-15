e29383f7dccfb836e3be4e21efcacfa52c0ab757
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-03-15 14:24:46 +0530

    dataset practice

---
+++ practice/01_real_style_dataset.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+data = {
+    "name":["ANUJ","RAHUL","SHIVAM","amit","sumit"],
+    "study_hours":[3,5,2,6,4],
+    "sleep":[7,6,8,5,7],
+    "marks":[65,80,50,90,70]
+}
+df = pd.DataFrame(data)
+print(df)
+
+# column selection 
+print(df["name"])
+
+# mutiple column selection
+print(df[["name","study_hours"]])
+
+# filtering data
+print(df[df["study_hours"]>3])
+
+# dataset summary
+print(df.describe())
+
+# features and labels 
+x=df[["study_hours","sleep"]]
+y=df["marks"]
+print(x)
+print(y)
+
